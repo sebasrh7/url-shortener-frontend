702322aaaf8d1fe94353167ac7a6e3cea5327288
sebasrh7 <sebastianrengifohidalgo@gmail.com>
2024-04-19 19:31:23 -0500

    test

--- src/services/guestUrl/guestUrlService.js
+++ src/services/guestUrl/guestUrlService.js
@@ -1,6 +1,7 @@
 const API = import.meta.env.VITE_API_URL;
 
 export const guestShorten = (data) => {
+  console.log(API);
   return fetch(`${API}/guest/shorten`, {
     method: "POST",
     headers: {
