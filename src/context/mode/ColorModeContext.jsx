a173002e5f0392d5d6905adb581f0299d36d72fb
sebasrh7 <sebastianrengifohidalgo@gmail.com>
2024-04-26 14:46:46 -0500

    responsive fontSize

--- src/context/mode/ColorModeContext.jsx
+++ src/context/mode/ColorModeContext.jsx
@@ -22,6 +22,48 @@ export const ColorModeProvider = ({ children }) => {
       palette: {
         mode,
       },
+      typography: {
+        fontFamily: ["Poppins", "sans-serif"].join(","),
+        h1: {
+          fontSize: "4.5rem",
+          "@media (max-width:900px)": {
+            fontSize: "4rem",
+          },
+          "@media (max-width:600px)": {
+            fontSize: "3rem",
+          },
+          "@media (max-width:400px)": {
+            fontSize: "2.5rem",
+          },
+        },
+        h6: {
+          fontSize: "1.5rem",
+          "@media (max-width:900px)": {
+            fontSize: "1.25rem",
+          },
+          "@media (max-width:600px)": {
+            fontSize: "1rem",
+          },
+        },
+        body1: {
+          fontSize: "1rem",
+          "@media (max-width:900px)": {
+            fontSize: "0.875rem",
+          },
+          "@media (max-width:600px)": {
+            fontSize: "0.75rem",
+          },
+        },
+        body2: {
+          fontSize: "0.875rem",
+          "@media (max-width:900px)": {
+            fontSize: "0.75rem",
+          },
+          "@media (max-width:600px)": {
+            fontSize: "0.625rem",
+          },
+        },
+      },
     });
   }, [mode]);
 
